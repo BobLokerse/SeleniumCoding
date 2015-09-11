73b8acee4767147d2f57db85ca24ebdcf6ec8b13
bob lokerse <bob.lokerse@gmail.com>
2015-09-11 21:04:06 +0200

    Adding Selenium example with PageObject pattern. With a unittest.

---
+++ SeleniumCode/Samples/PageObjects/GoogleHomePageObject.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+
+namespace Tahzoo.SeleniumCode.Samples.PageObjects
+{
+    // https://seleniumautomation84.wordpress.com/2014/03/06/page-object-model-tutorial-using-selenium-webdriver-in-c-visual-studio-and-vs-unittest-for-dummies/
+
+    public class GooglePage
+    {
+        public void OpenPage(string homepage, IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl(homepage);
+        }
+
+        public void SearchFor(string searchTerm, FirefoxDriver driver)
+        {
+            driver.FindElementByName("q").SendKeys(searchTerm);
+        }
+
+        public void ClickSearch(FirefoxDriver driver)
+        {
+            driver.FindElementByName("btnK").Click();
+        }
+
+        public void ClickFeelingLucky(FirefoxDriver driver)
+        {
+            driver.FindElementByName("btnI").Click();
+        }
+
+    }
+    
+}
---
+++ SeleniumCode_Tests/Samples/PageObjectTests/GooglePagePageObjectTest.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Tahzoo.SeleniumCode.Samples.PageObjects;
+
+
+
+namespace Tahzoo.SeleniumCode_Tests.Samples.PageObjectTests
+{
+    [TestClass]
+    public class GooglePagePageObjectTest
+    {
+        private IWebDriver _driver;
+
+
+        [TestMethod]
+        public void RunTests()
+        {
+            try
+            {
+                _driver = new FirefoxDriver();
+
+                string homepage = "http://google.co.uk";
+
+                string searchTerm = "Manual testing is long";
+
+                GooglePage pageObject = new GooglePage();
+
+                pageObject.OpenPage(homepage, (FirefoxDriver) _driver);
+                pageObject.SearchFor(searchTerm, (FirefoxDriver) _driver);
+                
+                // Does not work yet
+                // http://stackoverflow.com/questions/12082946/selenium-webdriver-org-openqa-selenium-elementnotvisibleexception-element-is-n
+                //pageObject.ClickSearch((FirefoxDriver) _driver);
+            }
+            finally
+            {
+                _driver.Close();
+            }
+
+        }
+    }
+
+}
