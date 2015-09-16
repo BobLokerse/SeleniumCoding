574174ebd466c699abd8f4b6ea90675ff7ddb900
bob lokerse <bob.lokerse@gmail.com>
2015-09-16 13:22:16 +0200

    Cheeselocator depend on (local)Website, removed for now. Add GooglePage fixture

---
+++ FitnesseFixtures/Fixtures/FixtureGooglePageWithServer.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using Tahzoo.SeleniumCode.Helpers;
+using Tahzoo.SeleniumCode.Samples.PageObjects;
+
+namespace Tahzoo.FitnesseFixtures.Fixtures
+{
+    public class FixtureGooglePageWithServer :
+        fit.ColumnFixture
+    {
+        private readonly IWebDriver _driver;
+        private GooglePage _googlePage;
+
+        public FixtureGooglePageWithServer()
+        {
+            _driver = Browser.GetFirefoxServerDriver();
+        }
+
+
+        public void OpenPage(string homepage)
+        {
+            _googlePage= new GooglePage(_driver);
+
+            _googlePage.OpenPage(homepage);
+        }
+
+        public void SearchFor(string searchTerm)
+        {
+            _googlePage.SearchFor(searchTerm);
+        }
+
+        public string PageTitle()
+        {
+            return _googlePage.PageTitle();
+        }
+
+        public void Close()
+        {
+            _googlePage.Close();
+        }
+    }
+}
