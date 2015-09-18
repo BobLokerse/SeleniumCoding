be3219a9f1bdae405756fb8e8888b4f98d03f6f0
bob lokerse <bob.lokerse@gmail.com>
2015-09-18 15:30:57 +0200

    Add Sear4Cheese fixture to Slim

---
+++ FitnesseFixtures/Fixtures/Slim/FixtureGooglePage.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using Tahzoo.SeleniumCode.Helpers;
+using Tahzoo.SeleniumCode.Samples.PageObjects;
+
+namespace Tahzoo.FitnesseFixtures.Fixtures.Slim
+{
+    public class FixtureGooglePage
+    {
+        #region Fields and Constructor
+        private readonly IWebDriver _driver;
+        private GooglePage _googlePage;
+
+        public FixtureGooglePage()
+        {
+            _driver = Browser.GetFirefoxDriver();
+        }
+        #endregion
+
+
+        public void OpenPage(string homepage)
+        {
+            _googlePage= new GooglePage(_driver);
+
+            _googlePage.OpenPage(homepage);
+        }
+
+        public void SearchFor(string searchTerm)
+        {
+            _googlePage.SearchFor(searchTerm);
+        }
+
+        public void ClickSearchButton()
+        {
+            _googlePage.ClickSearch();
+        }
+
+        public void ClickLuckyButton()
+        {
+            _googlePage.ClickLuckyButton();
+        }
+
+        public string PageTitle()
+        {
+            return _googlePage.PageTitle();
+        }
+
+        public void Close()
+        {
+            _googlePage.Close();
+        }
+    }
+}
---
+++ FitnesseFixtures/Fixtures/Slim/FixtureSearch4Cheese.cs
@@ -0,0 +1,19 @@
+﻿using Tahzoo.SeleniumCode.Samples;
+
+namespace Tahzoo.FitnesseFixtures.Fixtures.Slim
+{
+    public class FixtureSearch4Cheese
+    {
+        private string _resultTitle = null;
+
+        public void SearchForCheese()
+        {
+            _resultTitle = GoogleSuggest.Search4Cheese();
+        }
+
+        public string Title()
+        {
+            return _resultTitle;
+        }
+    }
+}
